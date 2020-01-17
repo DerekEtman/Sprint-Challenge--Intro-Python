cdd4a2b35a1174dd799245e5fa1d3ce15d226a30
Derek Etman <dereketman@gmail.com>
2020-01-17 08:08:11 -0800

    + oop1 finished

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -10,7 +10,52 @@
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
-#
+
+class  Vehicle:
+    def __init__(self):
+        pass
+
+    # This is the base class that is the top of the Hierarchy
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+    # Vehicle Attributes is passed into here
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+    # Vehicle and GroundVehicle Attributes are inherited here
+    # MotorCycle Attributes are not shared
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+    # Vehicle and GroundVehicle Attributes are inherited here
+    # Car Attributes are not Shared
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+    # Vehicle Attributes is inherited into here
+    # Ground Vehicle Attributes are not shared
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+    # Vehicle & Flight Vehicle Attributes are inherited into here
+    # Not shared with Startship
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+    # Vehicle & Flight Vehicle Attributes are inherited into here
+    # Not shared with Airplane
+
 # e.g.
 #
 # class Whatever:
