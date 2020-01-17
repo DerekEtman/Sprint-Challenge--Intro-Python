2f02aaf8cf2286c8eb0c980a94f695b9e3e14fa3
Derek Etman <dereketman@gmail.com>
2020-01-17 10:52:14 -0800

    Switched to Regex for alpha search

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -35,11 +35,11 @@ print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-
-
 print("Starts between C and G, inclusive:")
+import re
 
-c = [h.name for h in humans if h.name[0] in range(ord('C'), ord('G') + 1)]
+s = 'cdefgCDEFG'
+c = [h.name for h in humans if re.search(h.name[0], s)]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
